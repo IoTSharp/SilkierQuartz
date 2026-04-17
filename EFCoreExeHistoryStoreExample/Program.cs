104d680e7ef89a3d22998bf39443596c9a4cc9ca
maikebing <mysticboy@live.com>
2026-04-18 02:34:45 +0800

    升级依赖包并调整项目引用以增强兼容性
    本次提交主要内容：
    - EFCoreExeHistoryStoreExample 项目新增对 Quartz.Plugins.RecentHistory.EFCore 的引用，并指定迁移程序集。
    - SilkierQuartz.sln 移除 src 文件夹嵌套项目配置。
    - Quartz、Newtonsoft.Json 及多项测试相关依赖包升级，提升兼容性和功能支持。

--- EFCoreExeHistoryStoreExample/Program.cs
+++ EFCoreExeHistoryStoreExample/Program.cs
@@ -1,6 +1,7 @@
 using EFCoreExeHistoryStoreExample.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Quartz.Plugins.RecentHistory.EFCore;
 
 namespace EFCoreExeHistoryStoreExample
 {
@@ -13,7 +14,7 @@ namespace EFCoreExeHistoryStoreExample
             // Add services to the container.
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(connectionString));
+                options.UseSqlServer(connectionString,o=>o.MigrationsAssembly("Quartz.Plugins.RecentHistory.EFCore")));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
             builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
